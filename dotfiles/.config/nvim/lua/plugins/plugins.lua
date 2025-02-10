f0c804c5ba6ecd994dde16503eba859df8fed6a6
OpusMag <mlyngr@gmail.com>
2025-02-10 14:06:09 +0100

    Added nvim dotfiles

---
+++ dotfiles/.config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,39 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  -- Or with configuration
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme github_dark")
+    end,
+  },
+
+  {
+    "Lazyvim/Lazyvim",
+    opts = {
+      colorscheme = "github_dark",
+    },
+  },
+}
