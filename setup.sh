6819bf0dea5f40883c38b12db44ab35a1a1b0491
OpusMag <90897827+OpusMag@users.noreply.github.com>
2025-02-16 13:16:51 +0100

    Set up git config

--- setup.sh
+++ setup.sh
@@ -16,23 +16,27 @@ sudo apt update && sudo apt full-upgrade -y # it fails unless it's a full upgrad
 echo "Installing additional packages..."
 xargs -a package-list.txt sudo apt install -y
 
-# TODO: setup git
+echo "Setting up Git"
+read -p "Enter your Git user name: " git_user_name
+read -p "Enter your Git email: " git_user_email
+git config --global user.name "$git_user_name"
+git config --global user.email "$git_user_email"
+git config --global core.editor "nvim"
+git config --global init.defaultBranch main
 
 DOTFILES_REPO="https://github.com/OpusMag/dev-workflow-env.git"
 DOTFILES_DIR="$HOME/dev-workflow-env/dotfiles"
 
 # Running an install script so i don't have to deal with stowing
 if [ ! -d "$DOTFILES_DIR" ]; then
-    echo "Cloning dotfiles repository..."
+    echo "Cloning dotfiles repository"
     git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
     cd "$DOTFILES_DIR"
     ./stow.sh
 fi
 
-# TODO: Setup neovim config
-
 # Need to reload shell for changes to take effect
-echo "Reloading shell configuration..."
+echo "Reloading shell configuration"
 source ~/.bashrc
 
 echo "Setup complete! Please reboot for changes to take effect."
